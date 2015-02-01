02b3f4023bb5d297ccb6ebdb1897b52be5aa5529
Michael Ummels <michael@ummels.de>
2015-02-01 12:26:40 +0100

    Refactor tests
    Generate different types of priority maps
    Use two different orderings in tests
    Add tests for from and until

--- src/test/scala/de/ummels/prioritymap/PriorityMapSpec.scala
+++ src/test/scala/de/ummels/prioritymap/PriorityMapSpec.scala
@@ -1,58 +1,59 @@
 package de.ummels.prioritymap
 
 import org.scalacheck.{Arbitrary, Gen}
-import org.scalatest.{Matchers, PropSpec, prop}
+import org.scalatest.{Inspectors, Matchers, PropSpec, prop}
 
 import scala.collection.immutable.SortedSet
 
 /** Spec for immutable priority maps */
 class PriorityMapSpec extends PropSpec with prop.PropertyChecks with Matchers {
 
-  type Keys = Int
-  type Values = (Int, Int)
-
-  implicit val ord = Ordering.by[(Int, Int), Int](x => x._1)
+  import PriorityMapSpec._
 
   property("size should return the number of elements") {
-    forAll { (m: PriorityMap[Keys, Values]) =>
+    forAll(genPriorityMap) { m =>
       m.size shouldBe m.toSeq.size
     }
   }
 
   property("iterator should return items in priority order") {
-    forAll { (m: PriorityMap[Keys, Values]) =>
+    forAll(genPriorityMap) { m =>
       val values = m.values.toSeq
-      values shouldBe values.sorted
+      whenever(values.length > 1) {
+        Inspectors.forAll(values zip values.tail) { case (v1, v2) =>
+          m.ordering.lteq(v1, v2) shouldBe true
+        }
+      }
     }
   }
 
   property("head and tail should match") {
-    forAll { (m: PriorityMap[Keys, Values]) =>
+    forAll(genPriorityMap) { m =>
       if (m.nonEmpty) {
         val (h, t) = (m.head, m.tail)
         (h +: t.toSeq) shouldBe m.toSeq
       } else {
-        an [NoSuchElementException] should be thrownBy m.head
-        an [UnsupportedOperationException] should be thrownBy m.tail
+        an[NoSuchElementException] should be thrownBy m.head
+        an[UnsupportedOperationException] should be thrownBy m.tail
       }
     }
   }
 
   property("last and init should match") {
-    forAll { (m: PriorityMap[Keys, Values]) =>
+    forAll(genPriorityMap) { m =>
       if (m.nonEmpty) {
         val (l, i) = (m.last, m.init)
         (i.toSeq :+ l) shouldBe m.toSeq
       } else {
-        an [NoSuchElementException] should be thrownBy m.last
-        an [UnsupportedOperationException] should be thrownBy m.init
+        an[NoSuchElementException] should be thrownBy m.last
+        an[UnsupportedOperationException] should be thrownBy m.init
       }
     }
   }
 
   property("+ and ++ should add and replace elements") {
-    forAll (for {
-      m <- Arbitrary.arbitrary[PriorityMap[Keys, Values]]
+    forAll(for {
+      m <- genPriorityMap
       i <- Gen.choose(0, m.size)
     } yield (m, i)) { case (m, i) =>
       val kvs = m.toSeq
@@ -66,16 +67,16 @@ class PriorityMapSpec extends PropSpec with prop.PropertyChecks with Matchers {
   }
 
   property("updated should behave like +") {
-    forAll { (m: PriorityMap[Keys, Values], key: Keys, value: Values) =>
-      m updated (key, value) shouldBe m + (key -> value)
+    forAll(genPriorityMap, genKey, genValue) { (m, key, value) =>
+      m updated(key, value) shouldBe m + (key -> value)
     }
   }
 
   property("merged should merge old and new values") {
-    forAll (for {
-      m <- Arbitrary.arbitrary[PriorityMap[Keys, Values]]
-      keys <- Gen.listOfN(m.size, Arbitrary.arbitrary[Keys])
-      vals <- Gen.listOfN(m.size, Arbitrary.arbitrary[Values])
+    forAll(for {
+      m <- genPriorityMap
+      keys <- Gen.listOfN(m.size, genKey)
+      vals <- Gen.listOfN(m.size, genValue)
     } yield (m, keys, vals)) { case (m, keys, vals) =>
       val f: (Values, Values) => Values = (v1, v2) => (v1._1 + v2._1, v1._2 min v2._2)
       val kvs = keys zip vals
@@ -87,7 +88,7 @@ class PriorityMapSpec extends PropSpec with prop.PropertyChecks with Matchers {
   }
 
   property("- should remove elements") {
-    forAll { (m: PriorityMap[Keys, Values], key: Keys, value: Values) =>
+    forAll(genPriorityMap, genKey, genValue) { (m, key, value) =>
       val m1 = m - key
       if (!(m contains key)) {
         (m + (key -> value) - key) shouldBe m
@@ -101,83 +102,124 @@ class PriorityMapSpec extends PropSpec with prop.PropertyChecks with Matchers {
   }
 
   property("filterKeys should behave like filter") {
-    forAll (for {
-      m <- Arbitrary.arbitrary[PriorityMap[Keys, Values]]
+    forAll(for {
+      m <- genPriorityMap
       s <- Gen.choose(0, m.size)
       p <- Gen.containerOfN[Set, Keys](s, Gen.oneOf(m.keys.toSeq))
     } yield (m, p)) { case (m, p) =>
-      m.filterKeys(p) shouldBe m.filter{ case (k, _) => p(k) }
+      m.filterKeys(p) shouldBe m.filter { case (k, _) => p(k)}
     }
   }
 
   property("mapValues should behave like map") {
-    forAll (for {
-      m <- Arbitrary.arbitrary[PriorityMap[Keys, Values]]
+    forAll(for {
+      m <- genPriorityMap
       vals <- Gen.listOfN(m.size, Arbitrary.arbitrary[Int])
       f = Map.empty[Values, Int] ++ (m.values zip vals)
     } yield (m, f)) { case (m, f) =>
-      m.mapValues(f) shouldBe m.map{ case (k, v) => (k, f(v)) }
+      m.mapValues(f) shouldBe m.map { case (k, v) => (k, f(v))}
     }
   }
 
   property("firstKey should optionally return the first key") {
-    forAll { (m: PriorityMap[Keys, Values]) =>
+    forAll(genPriorityMap) { m =>
       m.firstKey shouldBe m.keys.headOption
     }
   }
 
   property("lastKey should optionally return the last key") {
-    forAll { (m: PriorityMap[Keys, Values]) =>
+    forAll(genPriorityMap) { m =>
       m.lastKey shouldBe m.keys.lastOption
     }
   }
 
   property("firstValue should optionally return the first value") {
-    forAll { (m: PriorityMap[Keys, Values]) =>
+    forAll(genPriorityMap) { m =>
       m.firstValue shouldBe m.values.headOption
     }
   }
 
   property("lastValue should optionally return the last value") {
-    forAll { (m: PriorityMap[Keys, Values]) =>
+    forAll(genPriorityMap) { m =>
       m.lastValue shouldBe m.values.lastOption
     }
   }
 
   property("valueSet should return the set of values") {
-    forAll { (m: PriorityMap[Keys, Values]) =>
-      m.valueSet shouldBe (SortedSet.empty ++ m.values)
+    forAll(genPriorityMap) { m =>
+      m.valueSet shouldBe (SortedSet.empty(m.ordering) ++ m.values)
     }
   }
 
   property("range should filter out entries with values outside the given bounds") {
-    forAll { (m: PriorityMap[Keys, Values], from: Option[Values], until: Option[Values]) =>
-      def isInRange(v: Values): Boolean =
-        from.map(f => m.ordering.lteq(f, v)).getOrElse(true) &&
-          until.map(u => m.ordering.lt(v, u)).getOrElse(true)
-      m.rangeImpl(from, until) shouldBe m.filter { case (_, v) => isInRange(v) }
+    forAll(genPriorityMap, genValue, genValue) { (m, from, until) =>
+      val ord = m.ordering
+      m.range(from, until) shouldBe m.filter { case (_, v) => ord.lteq(from, v) && ord.lt(v, until)}
+    }
+  }
+
+  property("from should filter out entries with small values") {
+    forAll(genPriorityMap, genValue) { (m, from) =>
+      m.from(from) shouldBe m.filter { case (_, v) => m.ordering.lteq(from, v)}
+    }
+  }
+
+  property("until should filter out entries with big values") {
+    forAll(genPriorityMap, genValue) { (m, until) =>
+      m.until(until) shouldBe m.filter { case (_, v) => m.ordering.lt(v, until)}
     }
   }
 
   property("withDefault should use the default function for keys not in the map") {
-    forAll { (m: PriorityMap[Keys, Values], d: Keys => Values, key: Keys) =>
+    forAll(genPriorityMap, genKey, genValue) { (m, key, value) =>
+      val d = (_: Keys) => value
       val m1 = m.withDefault(d)
       m1(key) shouldBe m.getOrElse(key, d(key))
       (m1 - key)(key) shouldBe d(key)
       m1.empty(key) shouldBe d(key)
-      (m1 filter { case (k, _) => k != key })(key) shouldBe d(key)
-      (m1 filterKeys { _ != key })(key) shouldBe d(key)
+      (m1 filter { case (k, _) => k != key})(key) shouldBe d(key)
+      (m1 filterKeys {
+        _ != key
+      })(key) shouldBe d(key)
     }
   }
 
   property("withDefaultValue should use the default function for keys not in the map") {
-    forAll { (m: PriorityMap[Keys, Values], d: Values, key: Keys) =>
+    forAll(genPriorityMap, genKey, genValue) { (m, key, d) =>
       val m1 = m.withDefaultValue(d)
       m1(key) shouldBe m.getOrElse(key, d)
       (m1 - key)(key) shouldBe d
       m1.empty(key) shouldBe d
-      (m1 filter { case (k, _) => k != key })(key) shouldBe d
-      (m1 filterKeys { _ != key })(key) shouldBe d
+      (m1 filter { case (k, _) => k != key})(key) shouldBe d
+      (m1 filterKeys {
+        _ != key
+      })(key) shouldBe d
     }
   }
+}
+
+object PriorityMapSpec {
+
+  type Keys = Int
+  type Values = (Int, Int)
+
+  val ord1 = Ordering.Tuple2(Ordering.Int, Ordering.Int)
+  val ord2 = Ordering.by[(Int, Int), Int](x => x._1)
+
+  def genKey: Gen[Keys] = Arbitrary.arbitrary[Keys]
+
+  def genValue: Gen[Values] = Arbitrary.arbitrary[Values]
+
+  def genPriorityMap: Gen[PriorityMap[Keys, Values]] = for {
+    ord <- Gen.oneOf(ord1, ord2)
+    elems <- Gen.listOf(Gen.zip(genKey, genValue))
+    m = PriorityMap.empty(ord) ++ elems
+    default <- Arbitrary.arbitrary[Option[Values]]
+    pred <- Arbitrary.arbitrary[Option[Keys => Boolean]]
+  } yield (default, pred) match {
+      case (None, None) => m
+      case (Some(d), None) => m withDefaultValue d
+      case (None, Some(p)) => m filterKeys p
+      case (Some(d), Some(p)) => (m withDefaultValue d) filterKeys p
+    }
 }
